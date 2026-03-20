9967304ff1872e4422703e10f8239f7d35618ce5
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-03-20 13:18:45 -0500

    added selection sort.

--- SortAlgorithms/SelectionSort.py
+++ SortAlgorithms/SelectionSort.py
@@ -3,3 +3,28 @@
     Time Complexity: O(n2) -> Quadratic Time
 
 '''
+
+def selection_sort(arr):
+    '''takes array and returns sorted.'''
+
+    arrlen = len(arr)
+
+    for i in range(arrlen):
+        minpos = i
+        
+        for j in range(minpos, arrlen):
+            
+            if arr[j] < arr[minpos]:
+                minpos = j
+    
+        arr[i], arr[minpos] = arr[minpos], arr[i]
+
+    return arr
+
+#------------------------------------------------------------
+
+
+#ignore, this is for testing the function in action. 
+sample = [4,6,3,7,8,2,4,7,3,9,5,4,3,]
+printer = selection_sort(sample)
+print(printer)
